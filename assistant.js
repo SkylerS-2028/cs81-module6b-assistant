1daf64aebb7f76a31aa2abfc3aab02ea6e2975cc
Skyler Stevenson <skylerstevenson@icloud.com>
2026-07-07 16:51:18 -0700

    added .addTask() method

--- assistant.js
+++ assistant.js
@@ -4,3 +4,9 @@ function PersonalAssistant(name){
     this.tasks = [];
     this.mood = null;
 }
+
+PersonalAssistant.prototype.addTask = function(task) {
+    this.tasks.push(task);
+    console.log(`Added ${task} task.`);
+}
+
