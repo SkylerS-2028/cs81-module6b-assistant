a78b2efc6a8012ba914ab1c7dfd66b2333e5aaab
Skyler Stevenson <skylerstevenson@icloud.com>
2026-07-07 17:12:05 -0700

    added mood features

--- assistant.js
+++ assistant.js
@@ -2,17 +2,29 @@
 function PersonalAssistant(name){
     this.name = name; 
     this.tasks = [];
-    this.mood = null;
+    this.mood = "content";
 }
 
 PersonalAssistant.prototype.addTask = function(task) {
     this.tasks.push(task);
     console.log(`Added '${task}' task.`);
+    if(this.tasks.length > 2){
+        this.mood = "busy";
+    }
+    else{
+        this.mood = "content";
+    }
 }
 
 PersonalAssistant.prototype.completeTask = function(){
     console.log(`You have completed '${this.tasks[0]}' task.`);
     this.tasks.shift();
+    if(this.tasks.length < 3){
+        this.mood = "accomplished";
+    }
+    else{
+        this.mood = "busy";
+    }
 }
 
 PersonalAssistant.prototype.reportMood = function(){
